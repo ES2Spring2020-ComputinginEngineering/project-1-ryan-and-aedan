fff8a1aed2e5caa1011ebbc7251e22f5f14f650a
aedanbrown <60242069+aedanbrown@users.noreply.github.com>
2020-02-29 10:47:26 -0500

    Added graphing function

--- Step 2/step1_theoretical_period.py
+++ Step 2/step1_theoretical_period.py
@@ -1,13 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Still need to write a comment explaining the real world limit of model
 
 lengths = [74.3, 63.9,54.5,45.4,35.6] #measured in centimeter
 arrlength = np.array(lengths)
 
 def length_to_period(length_array):
-    plt.plot(length_array,2*np.pi*np.sqrt((length_array/100)/9.81))
-    plt.show()
+
     return 2*np.pi*np.sqrt((length_array/100)/9.81)
 
 arrperiod = length_to_period(arrlength)
+
+def plotter(length_array,period_array):
+    plt.plot(length_array,2*np.pi*np.sqrt((length_array/100)/9.81))
+    plt.title("Title") ##Add title
+    plt.xlabel("Pendulum Length (cm)")
+    plt.ylabel("Period Length (s)")
+    plt.show()
+    
+plotter(arrlength,arrperiod)
