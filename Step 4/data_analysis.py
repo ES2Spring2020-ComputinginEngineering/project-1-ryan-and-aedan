f988537870f4a64799c7cdbfc46b68cfdb730037
aedanbrown <60242069+aedanbrown@users.noreply.github.com>
2020-02-29 16:40:50 -0500

    Updated data analysis

--- Step 4/data_analysis.py
+++ Step 4/data_analysis.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import csv
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal as sig
 
 #os.chdir(r"C:\Users\hanki\OneDrive\Documents\GitHub\hw1-ryan-hankins\project-1-ryan-and-aedan\Step 3")
 os.chdir(r"C:\Users\arb28\Desktop\School\Freshman\ES 2\Homework\project-1-ryan-and-aedan\Step 3")
@@ -33,18 +34,33 @@ for i in range(len(time)):
     
 angular_pos = np.arctan2(y_accel,z_accel)
     
-fig, axes = plt.subplots(3, sharex=True)
+fig, axes = plt.subplots(2, sharex=True)
 
 
 axes[0].plot(time,y_accel)
-axes[0].set_title("Y Accel")
+axes[0].plot(time,z_accel)
+axes[0].plot(time,x_accel)
+axes[0].set_title("Accel")
 axes[0].set(xlim=(0,60))
-axes[1].plot(time,z_accel)
-axes[1].set_title("Z Accel")
+axes[1].plot(time,angular_pos)
+axes[1].set_title("Ang Pos")
 axes[1].set(xlim=(0,60))
-axes[2].plot(time,angular_pos)
-axes[2].set_title("Ang Pos")
-axes[2].set(xlim=(0,60))
 fig.tight_layout()
+
+filtered_ang_pos = sig.medfilt(angular_pos)
+
+peak_array, _ = sig.find_peaks(filtered_ang_pos)
+
+period_length_arr = np.array([])
+
+for i in range(len(peak_array)-1):
+    
+    period_length_arr = np.append(period_length_arr, time[peak_array[i+1]] - time[peak_array[i]])
+
+average_period = period_length_arr.mean()
+
+print(average_period)
+    
+
 
 fin.close()
