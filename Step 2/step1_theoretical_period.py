d32e40e551ea8bea54ced909b5d89f416e171713
aedanbrown <60242069+aedanbrown@users.noreply.github.com>
2020-02-29 10:37:53 -0500

    Added graph

--- Step 2/step1_theoretical_period.py
+++ Step 2/step1_theoretical_period.py
@@ -1,11 +1,13 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 lengths = [74.3, 63.9,54.5,45.4,35.6] #measured in centimeter
 arrlength = np.array(lengths)
 
 def length_to_period(length_array):
+    plt.plot(length_array,2*np.pi*np.sqrt((length_array/100)/9.81))
+    plt.show()
     return 2*np.pi*np.sqrt((length_array/100)/9.81)
 
 arrperiod = length_to_period(arrlength)
