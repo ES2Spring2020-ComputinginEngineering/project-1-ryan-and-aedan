e11b15096a8c1dc820078bcca2dcbb5f1af24757
aedanbrown <60242069+aedanbrown@users.noreply.github.com>
2020-03-03 17:22:41 -0500

    Updated analysis

--- Step 4/data_analysis.py
+++ Step 4/data_analysis.py
@@ -9,11 +9,11 @@ import scipy.signal as sig
 #os.chdir(r"C:\Users\hanki\OneDrive\Documents\GitHub\hw1-ryan-hankins\project-1-ryan-and-aedan\Step 3")
 os.chdir(r"C:\Users\arb28\Desktop\School\Freshman\ES 2\Homework\project-1-ryan-and-aedan\Step 3")
 
-lengthlist = ['74.3']
+lengthlist = ['35.6','45.4','54.5','63.9','74.3']
 
 for i in lengthlist:
 
-    fin = open(i + 'cm.csv','r') #Add file name
+    fin = open(i + '.csv','r') #Add file name
     
     x_accel = np.array([])
     y_accel = np.array([])
@@ -32,11 +32,12 @@ for i in lengthlist:
             time = np.append(time,float(t[3]))
             
     time_initial = time[0]
+    
     for i in range(len(time)):
         time[i] = (time[i] - time_initial)/1000
         
         
-    angular_pos = np.arctan2(y_accel,z_accel)
+    angular_pos = np.arctan(z_accel/y_accel)
         
     fig, axes = plt.subplots(2, sharex=True)
     
@@ -45,25 +46,34 @@ for i in lengthlist:
     axes[0].plot(time,z_accel)
     axes[0].plot(time,x_accel)
     axes[0].set_title("Accel")
-    axes[0].set(xlim=(0,60))
+    axes[0].set(xlim=(2.5,60))
     axes[1].plot(time,angular_pos)
     axes[1].set_title("Ang Pos")
-    axes[1].set(xlim=(0,60))
+    axes[1].set(xlim=(2.5,60))
     fig.tight_layout()
     
     filtered_ang_pos = sig.medfilt(angular_pos)
     
-    peak_array, _ = sig.find_peaks(filtered_ang_pos)
+    #peak_array, _ = sig.find_peaks(filtered_ang_pos)
+    zero_array = np.array([])
+    for i in range(len(filtered_ang_pos)-1):
+        if filtered_ang_pos[i] < 0 and filtered_ang_pos[i + 1] > 0:
+            zero_array = np.append(zero_array, i)
     
     period_length_arr = np.array([])
     
-    for i in range(len(peak_array)-1):
-        
-        period_length_arr = np.append(period_length_arr, time[peak_array[i+1]] - time[peak_array[i]])
-    
+#    for i in range(len(peak_array)-1):
+#        
+#        period_length_arr = np.append(period_length_arr, time[peak_array[i+1]] - time[peak_array[i]])
+#    
+    for i in range(len(zero_array)-1):
+       
+        period_length_arr = np.append(period_length_arr, time[int(zero_array[i+1])] - time[int(zero_array[i])])
+   
     average_period = period_length_arr.mean()
     
     print(average_period)
+    
     
     
     fin.close()
